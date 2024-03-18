9de3ae60a9da39feeedc9a33d57d652ddfd5923b
Dijana Damchevska <77120285+damcevskad@users.noreply.github.com>
2024-03-18 16:10:09 +0100

    Sliding Tiles 3x3 (Greedy, A*)
    Given a random placement of the sliding tiles in an 8-puzzle, solve the problem of rearranging them in a row-major order. The sliding tiles are numbered from 1 to 8 on a 3x3 board where one of the squares is blank. You are permitted to slide the tiles either horizontally or vertically into the blank square. Use greedy search and A* search as informed (heuristic) search strategies to find a solution.

---
+++ sliding_tiles_puzzle.py
@@ -0,0 +1,72 @@
+from search import Problem, greedy_best_first_graph_search, astar_search
+
+class Puzzle(Problem):
+    """ The problem of sliding tiles numbered from 1 to 8 on a 3x3 board, where one of the
+    squares is a blank. A state is represented as a tuple of length 9, where  element at
+    index i represents the tile number  at index i (0 if it's an empty square) """
+
+    def __init__(self, initial, goal=(0,1, 2, 3, 4, 5, 6, 7, 8)):
+        """ Define goal state and initialize a problem """
+        super().__init__(initial, goal)
+
+    def find_blank_square(self, state):
+        """Return the index of the blank square in a given state"""
+
+        return state.index(0)
+
+    def actions(self, state):
+        """ Return the actions that can be executed in the given state.
+        The result would be a list, since there are only four possible actions
+        in any given state of the environment """
+
+        possible_actions = ['UP', 'DOWN', 'LEFT', 'RIGHT']
+        index_blank_square = self.find_blank_square(state)
+
+        if index_blank_square % 3 == 0:
+            possible_actions.remove('LEFT')
+        if index_blank_square < 3:
+            possible_actions.remove('UP')
+        if index_blank_square % 3 == 2:
+            possible_actions.remove('RIGHT')
+        if index_blank_square > 5:
+            possible_actions.remove('DOWN')
+
+        return possible_actions
+
+    def result(self, state, action):
+        """ Given state and action, return a new state that is the result of the action.
+        Action is assumed to be a valid action in the state """
+
+        # blank is the index of the blank square
+        blank = self.find_blank_square(state)
+        new_state = list(state)
+
+        delta = {'UP': -3, 'DOWN': 3, 'LEFT': -1, 'RIGHT': 1}
+        neighbor = blank + delta[action]
+        new_state[blank], new_state[neighbor] = new_state[neighbor], new_state[blank]
+
+        return tuple(new_state)
+
+    def goal_test(self, state):
+        """ Given a state, return True if state is a goal state or False, otherwise """
+
+        return state == self.goal
+
+    def h(self, node):
+        """ Return the heuristic value for a given state. Default heuristic function used is 
+        h(n) = number of misplaced tiles """
+
+        return sum(s != g for (s, g) in zip(node.state, self.goal))
+    
+if __name__ == '__main__':
+    puzzle = Puzzle((2, 4, 3, 1, 5, 6, 7, 8, 0))
+    result1 = greedy_best_first_graph_search(puzzle, puzzle.h)
+    print("\n Greedy Search Solution:")
+    print(result1.solution())
+
+    result2 = astar_search(puzzle, puzzle.h)
+    print("\n A* Search Solution:")
+    print(result2.solution())
+
+
+
